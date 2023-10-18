593be53fe9162e35055ae1cd3e571c78dc47506e
ezra <ezrayeneneh1992@gmail.com>
2023-10-18 04:21:55 -0700

    granting all permissions to a file

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- grant all permissions to a user created
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.*
+TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
