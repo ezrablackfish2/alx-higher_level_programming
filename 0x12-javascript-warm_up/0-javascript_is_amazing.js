eb5034700f70b05f4641da479362e20cb7426dd8
ezra <ezrayeneneh1992@gmail.com>
2023-11-13 11:10:23 -0800

    2 is done

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const ezra = 'JavaScript is amaing';
+const ezra = `JavaScript is amaing`;
 console.log(ezra);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-let ezra = `C is fun`;
-let shaib = `Python is cool`;
-let seid = `JavaScript is amazing`;
+const ezra = `C is fun`;
+const shaib = `Python is cool`;
+const seid = `JavaScript is amazing`;
 console.log(ezra);
 console.log(shaib);
 console.log(seid);
