2a49a27d971a5fee215c813de6d8a9b4053a7e56
ezra <ezrayeneneh1992@gmail.com>
2023-11-17 04:41:30 -0800

    we are doing good

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
+""" lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
 
-import MySQLdb;
-import sys;
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS states ( id INT NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY (id))")
-
-    states = ["California", "Arizona", "Texas", "New York", "Nevada"]
-    values = ", ".join(["('{}')".format(state) for state in states])
-    insert_query = "INSERT INTO states (name) VALUES {};".format(values)
-    cur.execute(insert_query)
-    cur.execute("SELECT * FROM states where name LIKE '{}'".format(sys.argv[4]))
-
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+        db = MySQLdb.connect(host="localhost", user=sys.argv[1],passwd=sys.argv[2], db=sys.argv[3], port=3306)
+        cur = db.cursor()
+        match = sys.argv[4]
+        cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
