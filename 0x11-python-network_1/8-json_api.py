939a252cd0e71457fde60ea42cb55f80b0f7a901
ezra <ezrayeneneh1992@gmail.com>
2023-12-01 05:45:09 -0800

    yes aman did it

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -14,6 +14,6 @@ if __name__ == "__main":
         if response == {}:
             print("No result")
         else:
-            print("[{}] {}".format(response.get("id", response.get("name"))))
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except ValueError:
         print("Not a valid JSON")
