0a5d018a7c5162aba8343079d4c2e0508c9a586b
ezra <ezrayeneneh1992@gmail.com>
2023-12-01 04:50:19 -0800

    the bordeom

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
+"""A script that:
+    - takes in a URL,
+    - sends a request to the URL
+    - displays the body of the response (decoded in utf-8).
+    """
 
 
-from urllib.request import Request, urlopen
-from urllib.error import URLError, HTTPError
-import sys
+    if __name__ == "__main__":
+            import sys
+            from urllib import request, error
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    req = Request(url)
-    try:
-        with response = urlopen(req) as response:
-            print(response.read().decode("utf-8"))
-    except error.HTTPError as e:
-        print("Error code: ", e.code)
+            try:
+                with request.urlopen(sys.argv[1]) as res:
+                    print(res.read().decode('UTF-8'))
+            except error.HTTPError as er:
+                print('Error code:', er.code)
