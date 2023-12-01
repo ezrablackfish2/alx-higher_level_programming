e30553ac943dd0d7ef0e7c9beefe54c1459b5783
ezra <ezrayeneneh1992@gmail.com>
2023-12-01 03:40:01 -0800

    yea nab

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
 req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
 with urllib.request.urlopen(req) as response:
     the_page = response.read()
-
-print("type: {}".format(type(html)))
-print("content: {}".format(html))
-print("utf8 content: {}".format(html.decode("utf-8")))
+print("Body response:$")
+print("\t- type: {}$".format(type(html)))
+print("\t- content: {}$".format(html))
+print("\t- utf8 content: {}$".format(html.decode("utf-8")))
