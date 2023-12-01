f7b5add3828c2bde1496b512b03b46794059ec65
ezra <ezrayeneneh1992@gmail.com>
2023-12-01 04:25:43 -0800

    data

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,10 +8,10 @@ import urllib.parse
 if __name__ == "__main__":
     data = {}
     data["email"] = sys.argv[2]
-    value = urllib.parse.urlencode(data).encode("ascii")
+    data = urllib.parse.urlencode(data).encode("ascii")
 
     url = sys.argv[1]
-    req = urllib.request.Request(url, value)
+    req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         the_page = response.read()
 
