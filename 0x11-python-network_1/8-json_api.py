46e9bc869cc8eb769e65cdc49bec3b5266803718
ezra <ezrayeneneh1992@gmail.com>
2023-12-01 05:30:13 -0800

    waw

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" this is it """
+
+import requests
+import sys
+
+if __name__ = "__main":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        reponse = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id", response.get("name"))))
+    except ValueError:
+        print("Not a valid JSON")
