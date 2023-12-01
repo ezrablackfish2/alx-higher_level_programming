2cd6541d532b1368b7016edbfe85c3407a7661bf
ezra <ezrayeneneh1992@gmail.com>
2023-12-01 04:53:31 -0800

    indentation error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,12 +6,12 @@
     """
 
 
-    if __name__ == "__main__":
-            import sys
-            from urllib import request, error
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
 
-            try:
-                with request.urlopen(sys.argv[1]) as res:
-                    print(res.read().decode('UTF-8'))
-            except error.HTTPError as er:
-                print('Error code:', er.code)
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
