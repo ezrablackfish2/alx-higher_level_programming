1e9e14ddb0e79c49705a60a779ac0eef6a3eb632
ezra <ezrayeneneh1992@gmail.com>
2023-12-01 05:52:11 -0800

    waw

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
         if response == {}:
             print("No result")
         else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
+            print("[{}] {}".format(response["id"], response["name"]))
     except ValueError:
         print("Not a valid JSON")
